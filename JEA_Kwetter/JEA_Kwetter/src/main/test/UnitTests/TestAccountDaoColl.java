c997ea65732f83c1803c2d1d2699d79ffe3bbe23
Violet Kampers <282404@student.fontys.nl>
2018-03-02 16:23:48 +0100

    Almost done with the Unit tests for now 0.o

--- JEA_Kwetter/JEA_Kwetter/src/main/test/UnitTests/TestAccountDaoColl.java
+++ JEA_Kwetter/JEA_Kwetter/src/main/test/UnitTests/TestAccountDaoColl.java
@@ -2,30 +2,40 @@ package UnitTests;
 
 import dao.AccountDaoColl;
 import domain.Account;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestAccountDaoColl {
 
-    private static AccountDaoColl accountDaoColl;
-    private static Account accountOne;
-    private static Account accountTwo;
-    private static Account accountThree;
+    private AccountDaoColl accountDaoColl;
+    private Account accountOne;
+    private Account accountTwo;
+    private Account accountThree;
 
-    private static List<Account> followers = null;
-    private static List<Account> followees = null;
+    private ArrayList<Account> followers = new ArrayList<>();
+    private ArrayList<Account> followees = new ArrayList<>();
 
-    @BeforeClass
-    public static void testSetupAccountDaoColl() {
+    private ArrayList<Account> followersEmpty = new ArrayList<>();
+    private ArrayList<Account> followeesEmpty = new ArrayList<>();
+
+    @Before
+    public void testSetupAccountDaoColl() {
         accountDaoColl = new AccountDaoColl();
 
         accountOne = new Account("Violet", followers, followees, "myWebsite", "Tilburg", "my bio");
-        accountTwo = new Account("Thomas", followers, followees, "myWebsite", "Tilburg", "my bio");
+        accountTwo = new Account("Thomas", followersEmpty, followeesEmpty, "myWebsite", "Tilburg", "my bio");
         accountThree = new Account("Roy", followers, followees, "myWebsite", "Tilburg", "my bio");
+
+        followers.add(accountTwo);
+        followers.add(accountThree);
+
+        followees.add(accountTwo);
+        followees.add(accountThree);
 
         accountDaoColl.addAccount(accountOne);
         accountDaoColl.addAccount(accountTwo);
@@ -46,36 +56,37 @@ public class TestAccountDaoColl {
 
     @Test
     public void testRemoveAccount_ExcistingAccount_RemovedTheAccount(){
-        
+        accountDaoColl.removeAccount(accountThree);
+        assertNull(accountDaoColl.findAccountByName("Roy"));
     }
 
     @Test
     public void testGetAllAccounts_ListWithAccount_TheExcistingList(){
-
+        assertEquals("Amount of found accounts is the same as the expected amount", 3, accountDaoColl.getAllAccounts().size());
     }
 
     @Test
     public void testFindAccountByName_ExcistingAcount_FoundTheExcistingAccount(){
-
+        assertEquals("Found account equals the excepted result", accountOne, accountDaoColl.findAccountByName("Violet"));
     }
 
     @Test
     public void testGetAllFollowersOfAccount_ExcistingAccount_FoundListOfFollowers(){
-
+        assertEquals("Found amount of followers equals the expected amount", 2, accountDaoColl.getAllFollowersOfAccount("Violet").size());
     }
 
     @Test
     public void testGetAllFollowersOfAccount_ExcistingAccountWithoutFollowers_FoundEmptyList(){
-
+        assertNull(accountDaoColl.getAllFollowersOfAccount("Thomas"));
     }
 
     @Test
     public void testGetAllFolloweesOfAccount_ExcistingAccount_FoundListOfFollowees(){
-
+        assertEquals(2, accountDaoColl.getAllFolloweesOfAccount("Violet").size());
     }
 
     @Test
     public void testGetAllFolloweesOfAccount_ExcistingAccountWithoutFollowees_FoundEmptyList(){
-
+        assertNull(accountDaoColl.getAllFolloweesOfAccount("Thomas"));
     }
 }
