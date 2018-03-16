6e4a36cfc55561737f231879fde191a3e15505fc
Violet Kampers <282404@student.fontys.nl>
2018-03-16 13:49:56 +0100

    Heartservice test uitgebreid

--- JEA_Kwetter/JEA_Kwetter/src/test/java/services/HeartServiceTest.java
+++ JEA_Kwetter/JEA_Kwetter/src/test/java/services/HeartServiceTest.java
@@ -5,10 +5,18 @@ import domain.Account;
 import domain.Heart;
 import domain.Message;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HeartServiceTest {
@@ -28,7 +36,25 @@ public class HeartServiceTest {
         heartService = new HeartService(heartsDao);
         account = new Account("first", null, null, "web", "location", "bio");
         owner = new Account("second", null, null, "web", "location", "bio");
-        message = new Message();
-        heart = new Heart(null, null);
+        message = new Message(owner, null, null, "text", null);
+        heart = new Heart(account, message);
+    }
+
+    @Test
+    public void getAllHeartsOfMessage_Message_GetAllHearts(){
+        List<Heart> heartExpected = new ArrayList<>();
+        heartExpected.add(heart);
+        when(heartService.getAllHeartsOfMessage(1L)).thenReturn(heartExpected);
+        List<Heart> heartFound = heartService.getAllHeartsOfMessage(1L);
+        assertThat(heartFound, is(heartExpected));
+    }
+
+    @Test
+    public void getAllHeartedAccountsOfMessage_Message_GetAllHearts(){
+        List<Heart> heartExpected = new ArrayList<>();
+        heartExpected.add(heart);
+        when(heartService.getAllHeartedAccountsOfMessage(1L)).thenReturn(heartExpected);
+        List<Heart> heartFound = heartService.getAllHeartsOfMessage(1L);
+        assertThat(heartFound, is(heartExpected));
     }
 }
