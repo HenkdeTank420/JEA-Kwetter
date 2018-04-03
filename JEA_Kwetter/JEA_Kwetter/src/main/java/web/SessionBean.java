74e3bd3072972c4ffe1dd61c07746bdc73acbb0d
Violet Kampers <282404@student.fontys.nl>
2018-04-03 13:46:22 +0200

    Something broke

--- JEA_Kwetter/JEA_Kwetter/src/main/java/web/SessionBean.java
+++ JEA_Kwetter/JEA_Kwetter/src/main/java/web/SessionBean.java
@@ -5,6 +5,7 @@ import domain.User;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import java.io.IOException;
 import java.io.Serializable;
 
 @Named
@@ -13,12 +14,16 @@ public class SessionBean implements Serializable{
 
     private User loggedInUser;
 
-    public String logout() {
+    public void logout() {
         loggedInUser = null;
 
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
 
-        return "/login?faces-redirect=true";
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/JEA_Kwetter");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public User getLoggedInUser() {
