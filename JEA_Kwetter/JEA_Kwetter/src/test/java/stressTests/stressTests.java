75268d625266ddb0e4300b81b226d33f30fa7a84
Violet Kampers <282404@student.fontys.nl>
2018-04-10 17:10:51 +0200

    stresstesting galore

--- JEA_Kwetter/JEA_Kwetter/src/test/java/stressTests/stressTests.java
+++ JEA_Kwetter/JEA_Kwetter/src/test/java/stressTests/stressTests.java
@@ -11,7 +11,7 @@ public class stressTests {
 
     @Test
     public void HashingStressTest() throws InvalidKeyException{
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 1000000; i++){
             EncryptionHelper.encryptData("Nyello");
         }
         Assert.assertTrue(true);
